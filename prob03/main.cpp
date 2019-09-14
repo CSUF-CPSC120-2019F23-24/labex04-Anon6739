c2ca340397248fabc99a33d7cb9afe763ebcd8d7
solisandrew117 <solisandrew117@cs104u-2d.acad.fullerton.edu>
2019-09-14 14:19:45 -0700

    Prediction

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,30 @@
 // Sales prediction
+#include <iostream>
+#include <string>
+
+int main ()
+{
+  // CompanyX sales in 2018 and predicted sales in 2019 with an 18% increase
+  int total_sales_2018 = 2103419277;
+
+  double percent_increase_2019 = 1.18;
+
+ // Calculating the predicted sales production in 2019
+  long sales_production_in_2019 = total_sales_2018 * percent_increase_2019;
+
+// Gathering the respected information to calcuate the predicted sales production in 2019
+  std::cout << "Last year's sales were $" << total_sales_2018 << std::endl;
+
+  std::cout << "This year's sales prediction: $" << sales_production_in_2019 << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+}
